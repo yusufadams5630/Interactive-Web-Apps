b38378aa608b62d708b4b7d43c35384300cfd94b
yusufadams5630 <101582190+yusufadams5630@users.noreply.github.com>
2022-06-05 18:23:36 +0200

    Initial commit

---
+++ JS/Javascript.js
@@ -0,0 +1,25 @@
+
+        console.log("\n\n ------- JS Basics and Debugging Challenge -------\n")            
+    
+        // The following 2 lines both have errors in them
+        console.log("Lesson 1: Logging a String to the console");
+        console.log("Hello World!");
+    
+        console.log("\n");
+        
+        // There is are 2 errors in the following lines
+        console.log("Lesson 2: This is how you create a variable:");
+        let number = 86;
+        console.log(number);
+    
+        console.log("\n");
+    
+        // The plus sign is not showing, debug this and have it appear inside the String
+        console.log("Lesson 3 is concatinating Strings using the ' + ' sign." );
+    
+        // These variables are undefined and breaking the String that is using them 
+        let concatString = "Strings";
+        let concatVar = "variables";
+        console.log("You can concatenate " + concatVar + " into " + concatString + " to log them into the console");
+
+        
